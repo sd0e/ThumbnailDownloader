97f4f8c7b6c0774eb5aa290f5d3d6a2bb68e965c
Sebastian Doe <39373619+sebastiandoe5@users.noreply.github.com>
2019-10-30 16:05:35 +0000

    Add URL parsing!

---
+++ parser.js
@@ -0,0 +1,8 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
+}
+
+function loadYTURL() {
+    var url = getURLParameter("url");
+    document.getElementById("urlInput").value = url;
+}
